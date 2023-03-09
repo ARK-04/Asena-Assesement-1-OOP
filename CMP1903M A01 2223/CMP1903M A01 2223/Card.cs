22376f7de98f7ae11f22ef487a2314cc2d2f58ab
ARK-04 <125880303+ARK-04@users.noreply.github.com>
2023-03-09 11:10:51 +0000

    Add files via upload

---
+++ CMP1903M A01 2223/CMP1903M A01 2223/Card.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMP1903M_A01_2223
+{
+    public enum Suit //to set the suit of the card
+    {
+        Club = 1,
+        Diamond = 2,
+        Heart = 3,
+        Spades = 4,
+    }
+
+    public enum Value //to set the value of the card
+    {
+        Ace = 1,
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        Jack = 11,
+        Queen = 12,
+        King = 13,
+    }
+    class Card
+    {
+        public string cardValue { get; }
+
+        public String value
+        { get { return cardValue; } }
+
+        public Card(int packSuit, int packValue)
+        {
+            var suitText = (Suit)packSuit;
+            var valueText = (Value)packValue;
+            cardValue = (suitText + " " + valueText);
+        }
+    }
+}
